e55c9973c6d6a9afc14f6a27d3dd1ea479ae5886
Max Escher <max@maxescher.com>
2020-04-21 00:56:35 -0400

    Add and configure eslint

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import react from 'react';
-
 export default function Index() {
   return ;
 }
