79a86190331345f83dd02fa73f2d85eaf8749296
Max Escher <max@maxescher.com>
2021-06-19 04:12:22 -0400

    refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,42 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-
-
 module.exports = (env) => {
-  
-  const rules = [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
-      }
-    },
-    {
-      test: /\.s[ac]ss$/i,
-      exclude: /node_modules/,
-      include: [
-        path.resolve(__dirname, 'src/styles')
-      ],
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    }
-  ]
+  const module = {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        include: [path.resolve(__dirname, 'src/styles')],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  };
 
   const plugins = [
     new HTMLWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html',
     }),
     new CopyPlugin({
-      patterns: [
-        { from: './static', to: './' }
-      ]
-    })
-  ]
+      patterns: [{ from: './static', to: './' }],
+    }),
+  ];
 
   return {
     entry: path.join(__dirname, 'src', 'index.js'),
     output: {
       filename: 'bundle.js',
-      path: path.resolve(__dirname, './build')
+      path: path.resolve(__dirname, './build'),
     },
-    module: { rules },
+    module: module,
     stats: 'minimal',
     plugins: plugins,
-  }
-}
+  };
+};
